39c5db05bc6cab120dbe3b66108108c69063c799
berenboden <beren.boden@gmail.com>
2023-12-03 08:59:29 +1300

    very simple function in C

---
+++ printing.c
@@ -0,0 +1,9 @@
+//Include built in standard input and output.
+#include <stdio.h>
+
+//Calling main function to call print with a string as input
+main()
+{
+    printf("hello, world\n");
+}
+//Compile with `cc printing.c` then run `./a.out`.
